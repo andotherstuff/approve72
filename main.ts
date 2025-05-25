3f3a33138d9650466755b50e4b3692f8cdcd6cf2
Alex Gleason <alex@alexgleason.me>
2025-05-25 09:18:57 +0200

    Add the approver bot

--- main.ts
+++ main.ts
@@ -1,8 +1,53 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { NRelay1, NSecSigner } from "@nostrify/nostrify";
+import { nip19 } from "nostr-tools";
+import { parseArgs } from "@std/cli/parse-args";
+
+const flags = parseArgs(Deno.args, {
+  string: ["nsec", "a", "r"],
+});
+
+const decoded = nip19.decode(flags.nsec!);
+
+if (decoded.type !== "nsec") {
+  throw new Error("Invalid nsec");
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+const nsec = decoded.data;
+
+if (!flags.a) {
+  throw new Error("Missing -a tag for the group");
+}
+
+if (!flags.r) {
+  throw new Error("Missing -r relay URL");
+}
+
+const relay = new NRelay1(flags.r);
+const signer = new NSecSigner(nsec);
+const pubkey = await signer.getPublicKey();
+
+for await (const msg of relay.req([{ kinds: [4552], "#a": [flags.a] }])) {
+  if (msg[0] === "EVENT") {
+    const [_, _subId, event] = msg;
+
+    let [list] = await relay.query([{
+      kinds: [34551],
+      "#d": [flags.a],
+      authors: [pubkey],
+    }]);
+
+    if (list) {
+      list.tags.push(["p", event.pubkey]);
+      list = await signer.signEvent(list);
+    } else {
+      list = await signer.signEvent({
+        kind: 34551,
+        tags: [["d", flags.a], ["p", event.pubkey]],
+        content: "",
+        created_at: Math.floor(Date.now() / 1000),
+      });
+    }
+
+    await relay.event(list);
+  }
 }
